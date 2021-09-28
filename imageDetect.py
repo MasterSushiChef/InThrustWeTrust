08fe5886b91e7e6ba52df97eaef6840ed455ed81
wglao <70598852+wglao@users.noreply.github.com>
2021-09-28 10:57:04 -0500

    started corner detection

--- imageDetect.py
+++ imageDetect.py
@@ -17,30 +17,7 @@ def angles(vec1,vec2):
 # endDef
 
 ### Function for detecting yellowness
-def yellowness(im):
-    try:
-        imarr = np.array(im).astype(float)
-        yellowim = np.copy(imarr)
-
-        # define unit yellow vector in bgr space
-        yellow = np.array([0,1,1])
-        
-        # test angle between pixel color and yellow vector
-        # set to white if within pi/9 rad
-        for col in range(yellowim.shape[0]):
-            for row in range(yellowim.shape[1]):
-                # if angles(yellow,im[col,row,:]) < np.pi/4 and im[col,row,0]/la.norm(im[col,row,:]) < 1/4:
-                if angles(yellow,im[col,row,:]) < np.pi/9 and la.norm(im[col,row,:]) > la.norm(np.array([80,80,0])):
-                    yellowim[col,row] = 1
-                else:
-                    yellowim[col,row] = 0
-        yelcv = np.array(yellowim * 255, dtype = np.uint8)
-        cv2.imshow("yellowness",yelcv)
-    except Exception as e:
-        print(e)
-# endDef
-
-### convert to hsv and test for yellow
+# convert to hsv and test for yellow
 def yellowhsv(im):
     # convert im to hsv space
     hsvim = cv2.cvtColor(im, cv2.COLOR_BGR2HSV)
@@ -48,33 +25,70 @@ def yellowhsv(im):
     yelmax = np.array([80,255,255])
     mask = cv2.inRange(hsvim, yelmin, yelmax)
     cv2.imshow("mask", mask)
-    kernel = np.ones((3,3),np.uint8)
+    kernel = np.ones((5,5),np.uint8)
     mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
+    kernel = np.ones((3,3),np.uint8)
+    mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
     contours, hierarchy = cv2.findContours(mask,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-    cv2.drawContours(mask,contours,-1,(100,100,100),3)
-    # for i in range(3,7,2):
-    #     kernel = np.ones((i,i),np.uint8)
-    #     mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
-    #     kernel = np.ones((i,i),np.uint8)
-    #     mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
-    
-    # cv2.imshow("noise removed", mask)
-    cv2.imshow("contour", mask)
+    if len(contours) != 0:
+        cv2.drawContours(im,contours,-1,(255,0,0),3)
+        # find the biggest countour (c) by the area
+        c = max(contours, key = cv2.contourArea)
+        x,y,w,h = cv2.boundingRect(c)
+        # extract subimage for corner detection
+        subim = mask[y:y+h,x:x+w]
+        print(np.max(subim))
+        cv2.imshow("subimage", subim)
+        # draw the biggest contour (c) in green
+        cv2.rectangle(im,(x,y),(x+w,y+h),(0,255,0),2)
+        cv2.imshow("contour", im)
+    return im, mask, contours, subim
+# endDef
 
-    # binarize by yellow
-    pass
+def frownCheck(im):
+    # smooth edges
+    cv2.imshow("rough", im)
+    im = cv2.medianBlur(im, 5)
+    cv2.imshow("filter", im)
+    # find corners
+    dst = cv2.cornerHarris(im,2,3,0.04)
+    dst = cv2.dilate(dst,None)
+    ret, dst = cv2.threshold(dst,0.01*dst.max(),255,0)
+    dst = np.uint8(dst)
+
+    # find centroids
+    ret, labels, stats, centroids = cv2.connectedComponentsWithStats(dst)
+
+    # define the criteria to stop and refine the corners
+    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 100, 0.001)
+    corners = cv2.cornerSubPix(im,np.float32(centroids),(5,5),(-1,-1),criteria)
+
+    # Now draw them
+    res = np.hstack((centroids,corners))
+    res = np.int0(res)
+    im = cv2.cvtColor(im,cv2.COLOR_GRAY2BGR)
+    for i in res:
+        cv2.circle(im,(i[1],i[0]),3,[0,255,0],-1)
+        cv2.circle(im,(i[2],i[3]),3,[0,0,255],-1)
+    if res.shape[0]>24:
+        return im, corners, True
+    else:
+        return im, corners, False
 # endDef
 
 if __name__ == "__main__":
     imageDir = "C:\\Users\\wesle\\Documents\\ASE 361L\\Image Rec"
-    imfile = os.path.join(imageDir,"FrownCloseUp.png")
-    # imfile = os.path.join(imageDir,"FrownMedium.png")
+    # imfile = os.path.join(imageDir,"Smile_Wide.png")
+    imfile = os.path.join(imageDir,"Frown_Wide.png")
     if os.path.isfile(imfile):
             im = cv2.imread(imfile)
-            cv2.imshow("full-scale",im)
-            # smallim = cv2.resize(im, (int(im.shape[1]/2), int(im.shape[0]/2)), interpolation=cv2.INTER_AREA)
-            # cv2.imshow("reduced",smallim)
-    # yellowness(smallim)
-    yellowhsv(im)
+            # cv2.imshow("full-scale",im)
+            smallim = cv2.resize(im, (int(im.shape[1]/6), int(im.shape[0]/6)), interpolation=cv2.INTER_AREA)
+            cv2.imshow("reduced",smallim)
+    traceim, mask, contours, subim = yellowhsv(smallim)
+    subim, corners, frown = frownCheck(subim)
+    print(frown)
+    cv2.imshow("corners",subim)
+    # test for frown
     cv2.waitKey(0)
     
